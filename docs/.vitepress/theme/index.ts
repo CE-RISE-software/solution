8ce5b4a640062a699aaef367618aec4b0cd7561f
Riccardo Boero <ribo@nilu.no>
2026-03-19 06:25:49 -0400

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,10 +4,14 @@ import type { Theme } from "vitepress";
 import "./custom.css";
 import PageCards from "./components/PageCards.vue";
 import PageIntro from "./components/PageIntro.vue";
+import SiteFooter from "./components/SiteFooter.vue";
 
 const theme: Theme = {
     extends: DefaultTheme,
-    Layout: () => h(DefaultTheme.Layout, null, {}),
+    Layout: () =>
+        h(DefaultTheme.Layout, null, {
+            "layout-bottom": () => h(SiteFooter),
+        }),
     enhanceApp(ctx) {
         DefaultTheme.enhanceApp?.(ctx);
         ctx.app.component("PageCards", PageCards);
