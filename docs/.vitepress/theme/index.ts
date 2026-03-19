24ca35f296d5adaaf321f8813148997982ea3a68
Riccardo Boero <ribo@nilu.no>
2026-03-19 06:11:44 -0400

    debugging

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
+import "./custom.css";
 import PageCards from "./components/PageCards.vue";
 import PageIntro from "./components/PageIntro.vue";
 
