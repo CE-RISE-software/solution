0209881b069823c55d89e785adc274509559ec6e
Riccardo Boero <ribo@nilu.no>
2026-03-19 06:08:30 -0400

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 import { h } from "vue";
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
-import "./custom.css";
 import PageCards from "./components/PageCards.vue";
 import PageIntro from "./components/PageIntro.vue";
 
