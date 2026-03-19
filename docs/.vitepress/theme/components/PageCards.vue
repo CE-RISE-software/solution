a17522dff3f4a884e33b114ecee2fa30f9cde82c
Riccardo Boero <ribo@nilu.no>
2026-03-19 08:03:26 -0400

    reestablishing confs

--- docs/.vitepress/theme/components/PageCards.vue
+++ docs/.vitepress/theme/components/PageCards.vue
@@ -12,5 +12,16 @@ defineProps<{
 </script>
 
 <template>
-  <!-- PageCards disabled -->
+  <div class="ce-card-grid" :data-tone="tone ?? 'blue'">
+    <component
+      :is="item.link ? 'a' : 'article'"
+      v-for="item in items"
+      :key="item.title"
+      class="ce-card-grid__item"
+      :href="item.link ? withBase(item.link) : undefined"
+    >
+      <h3>{{ item.title }}</h3>
+      <p>{{ item.text }}</p>
+    </component>
+  </div>
 </template>
