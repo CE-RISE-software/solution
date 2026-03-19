8bb6e534afc3efb691444f0157189e4f0b7f4872
Riccardo Boero <ribo@nilu.no>
2026-03-19 05:45:38 -0400

    Update PageCards.vue

--- docs/.vitepress/theme/components/PageCards.vue
+++ docs/.vitepress/theme/components/PageCards.vue
@@ -12,16 +12,5 @@ defineProps<{
 </script>
 
 <template>
-  <div class="ce-card-grid" :data-tone="tone ?? 'blue'">
-    <component
-      :is="item.link ? 'a' : 'article'"
-      v-for="item in items"
-      :key="item.title"
-      class="ce-card-grid__item"
-      :href="item.link ? withBase(item.link) : undefined"
-    >
-      <h3>{{ item.title }}</h3>
-      <p>{{ item.text }}</p>
-    </component>
-  </div>
+  <!-- PageCards disabled -->
 </template>
