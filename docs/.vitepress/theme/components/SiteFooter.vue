53d454de723388bb347e6ad8aefd772ecef2e12c
Riccardo Boero <ribo@nilu.no>
2026-03-19 06:35:01 -0400

    Update SiteFooter.vue

--- docs/.vitepress/theme/components/SiteFooter.vue
+++ docs/.vitepress/theme/components/SiteFooter.vue
@@ -1,34 +1,5 @@
-<script setup lang="ts">
-import { withBase } from "vitepress";
-</script>
-
 <template>
   <footer class="ce-site-footer">
-    <div class="ce-site-footer__inner">
-      <div class="ce-site-footer__brand">
-        <img class="ce-site-footer__logo" :src="withBase('/images/CE-RISE_logo.png')" alt="CE-RISE logo" />
-      </div>
-
-      <div class="ce-site-footer__columns">
-        <section>
-          <h2>Documentation</h2>
-          <ul>
-            <li><a :href="withBase('/overview')">Overview</a></li>
-            <li><a :href="withBase('/architecture')">Architecture</a></li>
-            <li><a :href="withBase('/data-models')">Data models</a></li>
-          </ul>
-        </section>
-
-        <section>
-          <h2>Project</h2>
-          <ul>
-            <li><a href="https://ce-rise.eu/" target="_blank" rel="noreferrer">CE-RISE Website</a></li>
-            <li><a href="https://zenodo.org/communities/ce-rise/" target="_blank" rel="noreferrer">Zenodo Archive</a></li>
-            <li><a href="https://codeberg.org/CE-RISE-models" target="_blank" rel="noreferrer">Data Models</a></li>
-            <li><a href="https://codeberg.org/CE-RISE-software" target="_blank" rel="noreferrer">Software</a></li>
-          </ul>
-        </section>
-      </div>
-    </div>
+    <div class="ce-site-footer__inner"></div>
   </footer>
 </template>
